552fad22bd8e4f97d9fc85404c414a56ec633343
Val Gabriel <gabriel.valryan@hotmail.com>
2018-06-13 10:00:25 -0500

    Inquirer Validations

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require('inquirer');
+var Table = require('cli-table');
+var validator = require('validator');
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -19,33 +21,53 @@ function afterConnection() {
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
 
-    console.log("-------------------------------------------------------------");
-    console.log("-----------------------ITEMS FOR SALE------------------------");
-    console.log("-------------------------------------------------------------");
-    for(var i = 0; i < res.length; i++)
-    {
-        console.log("ITEM ID: " + res[i].item_id + " | " + 
-                    "ITEM NAME: " + res[i].product_name + " | " + 
-                    "DEPARTMENT: " + res[i].department_name + " | " +
-                    "PRICE: " + res[i].price + " | " +
-                    "QUANTITY: " + res[i].stock_quantity);
-        console.log("----------------------------------------------------")
+    var table = new Table({
+      head: ["ID", "DEPARTMENT", "ITEM NAME", "PRICE", "QUANTITY"],
+      colWidths: [5, 15, 30, 10, 10]
+    });
+
+    for (var i = 0; i < res.length; i++) {
+      table.push(
+        [res[i].item_id, res[i].department_name, res[i].product_name, res[i].price, res[i].stock_quantity]
+      );
     }
+  
+    console.log(table.toString());
 
     inquirer.prompt([
-
-        {
-          type: "input",
-          name: "id",
-          message: "What is the ID of the product they would like to buy?",
-        },
-        {
-          type: "input",
-          name: "units",
-          message: "How many units of the product they would like to buy?",
+      {
+      type: "input",
+      message: "Enter the ID of the product they would like to buy",
+      name: 'id',
+      validate: function(id) {
+        if (parseInt(id) != 0 && parseInt(id) <= res.length) {
+          return true;
         }
-    ]);
-    
+        else {
+          console.log("\n\nInvalid Input. Please double check your Item's ID and try again.\n");
+          return false;
+        }
+      }
+      },
+      {
+      type: "input",
+      message: "\nHow many units of the product they would like to buy?",
+      name: "units",
+      validate: function(units) {
+        var val = validator.isNumeric(units);
+
+        if (val) {
+          return true;
+        }
+        else {
+          console.log("\n\nInvalid Input. Please double check your Item's ID and try again.\n");
+          return false;
+        }
+      }
+      },
+    ])
     connection.end();
-  });
-}
+  }
+)};
+
+
