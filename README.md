fe7be750aa3160e50b34bad2d3461f02502e307a
Val Gabriel <gabriel.valryan@hotmail.com>
2018-06-29 12:34:17 -0500

    read me

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Bamazon
+Bamazon
+
+BamazonCustomer.js
+-     Displays the products in the store.
+-     Displays a prompt to the customer asking which product they would like to 
+purchase by ID number.
+-	Asks for the quantity.
+
+![Image of Customer.js](./img.png);
+ 
+
+
+
+
+
--- bamazonManager.js
+++ bamazonManager.js
@@ -9,7 +9,7 @@ var connection = mysql.createConnection({
     user: "root",
     password: "password",
     database: 'bamazon'
-})
+});
 
 // Prompts User or Menu Option
 function prompt() {
@@ -17,7 +17,7 @@ function prompt() {
         [
             {
                 type: "list",
-                message: "Select one of the following menu options",
+                message: "\n\nSelect one of the following menu options",
                 choices: ["View Products for Sale",
                         "View Low Inventory",
                         "Add to Inventory",
@@ -43,6 +43,7 @@ function prompt() {
             
             case "Add New Product":
                 addPro();
+                break;
             
             default:
                 console.log("You have now Exited the program");
@@ -78,15 +79,82 @@ function display() {
 }
 
 function lowInventory() {
+    connection.query("SELECT * FROM products WHERE stock_quantity <= 5", function(err, res) {
+        if (err) throw err;
 
-    
-}
-
-function addInv() {
-
-}
+        var table = new Table({
+            head: ["ID", "DEPARTMENT", "ITEM NAME", "PRICE", "QUANTITY"],
+            colWidths: [5, 15, 30, 10, 10] 
+          });
+      
+          // Pushes Data from Query Response to array
+          for (var i = 0; i < res.length; i++) {
+            table.push(
+              [res[i].item_id, res[i].department_name, res[i].product_name, res[i].price, res[i].stock_quantity]
+            );
+          }
+          // Display table with appropriate data
+          console.log(table.toString());
+          prompt();
+    }) // end of query
+}// end of lowInven function
 
 function addPro() {
+    inquirer.prompt(
+        [
+            {
+                type: "list",
+                message: "Please Select the Department: ",
+                choices: ['Food', 'Clothing', 'Technology', 'Pets', 'Furniture'],
+                name: "department"
+            },
+            {
+                type: "input",
+                message: "Enter item name: ",
+                name: "item_name"
+            },
+            {
+                type: "input",
+                message: "Enter price",
+                name: "price"   
+            },
+            {
+                type: "input",
+                message: "Enter How many are in stock: ",
+                name: "units"
+            },
+            {
+                type: "confirm",
+                message: "Would you like to add this new item?",
+                name: "confirm",
+                default: true
+            }
+        ]
+    )
+    .then(function(res) {
+        if (res.confirm) {
+            var dept = res.department;
+            var item = res.item_name;
+            var price = parseInt(res.price);
+            var units = parseInt(res.units);
+            var queryState = "INSERT INTO products(product_name, department_name, price, stock_quantity) " + "VALUES (" + item + ", " + dept + ", " + price + ", " + units + ")";
+            
+            connection.query(queryState, function(err, res) {
+                console.log("Item Successfully added.")
+                console.log("\nITEM: " + item + "\n",
+                            "DEPARTMENT: " + dept + "\n",
+                            "PRICE: " + price + "\n",
+                            "QUANTITY: " + units + "\n");
+                display();
+            })
+        }
+        else {
+            prompt();
+        }
+    })
+}// END addInv(); function.
+
+function addInv() {
 
 }
 
